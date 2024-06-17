2b3ec1f7a026a0acda8e6972081195b038c8f911
najerans <najera.lsc@outlook.com>
2024-06-17 13:38:24 -0600

    clases

---
+++ clases.py
@@ -0,0 +1,23 @@
+"""
+class Usuario:
+    pass
+
+cody = Usuario()
+print (cody)
+
+"""
+
+#atributos de clase
+
+class Usuario:
+    username =" Username por default" #atributos dentro de la clase 
+    email =""
+
+print(Usuario.username)
+#     clase.atributo
+
+Usuario.username = "User 1"
+print(Usuario.username)
+
+
+#atributos de instancia 
